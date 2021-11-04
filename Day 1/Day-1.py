4a90747ff4cf5d1f571f0ca9583979e16cbe87bc
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-11-04 16:31:50 +0530

    Day 1

---
+++ Day 1/Day-1.py
@@ -0,0 +1,39 @@
+# Printing
+#Day 1-1- Exercise
+print("Day 1 - Python Print Function")
+print("The function is declared like this: ")
+print('print("what to print")')
+
+print("Hello World\nHello World")
+print("Hello" + " Hem")
+
+
+#Day-1-2- Exercise
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+
+
+#Day-1-3- Exercise
+
+name = input("Enter name to calculate its length\n")
+name_length = len(name)
+print("The length is " + str(name_length))
+
+
+#Day-1-4- Exercise
+
+a = input("a: ")
+b = input("b: ")
+temp = a
+a = b
+b = temp
+print("a: " + a)
+print("b: " + b)
+
+
+
