d5333c5b2ab0db5a69264a73d276fa76487cc840
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-11-05 13:28:01 +0530

    Day 2

--- Day 2/2-project.py
+++ Day 2/2-project.py
@@ -4,7 +4,7 @@ tip_percent = float(input("How much tip would you like to give? 10, 12, or 15? "
 total_people = int(input("How many people to split the bill? "))
 
 total_bill=  float((tip_percent * bill)/100 + bill)
-total_contribution = total_bill/total_people
+total_contribution = round(total_bill/total_people, 2)
 print(f"Each person should pay: ${total_contribution}")
 
 
