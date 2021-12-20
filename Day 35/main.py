367a89acd9b79ae679c4ca3531ad5b88e9a63443
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-12-20 09:16:27 +0530

    Day:35

---
+++ Day 35/main.py
@@ -0,0 +1,44 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+api_key = ''  #os.environ.get('')
+api_end_point = "https://api.openweathermap.org/data/2.5/onecall"
+account_sid = 'AC90ea8a134dc896d037599c43b331c745'
+auth_token = ''
+
+
+parameters = {
+    "lat": 18.520430,
+    "lon": 73.856743,
+    "appid": api_key,
+    "exclude": "current,minutely,daily,alerts"
+}
+
+
+data = requests.get(api_end_point, params=parameters)
+data.raise_for_status()
+
+weather_data = data.json()
+weather_slice = weather_data['hourly'][:12]
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code)< 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token , http_client=proxy_client)  
+    message = client.messages \
+                    .create(
+                        body="Bring an umbrella",
+                        from_='',
+                        to=''
+                    )
+
+    print(message.status)
