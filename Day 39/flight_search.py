c5621df5f4ec34f3405836fa6cdf82542f7a0e93
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2022-01-05 21:02:11 +0530

    Day 39

---
+++ Day 39/flight_search.py
@@ -0,0 +1,13 @@
+
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = 'HsqhcPHoe7uSgD_wzulJ-9OL2bJa_kjT'
+
+
+class FlightSearch:
+    
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
+
---
+++ Day 39/notification_manager.py
@@ -0,0 +1,7 @@
+ACCOUNT_SID = 'AC90ea8a134dc896d037599c43b331c745'
+AUTH_TOKEN = 'e80296aaad30a952d38f4ec0be676952'
+PHONE_NUMBER  = '+13092206108'
+
+class NotificationManager:
+    #This class is responsible for sending notifications with the deal flight details.
+    pass
