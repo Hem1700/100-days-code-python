08f022bafa97551abd34dd8a1a1c561ff20766b3
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2022-01-07 12:53:45 +0530

    Day 40

--- Day 39/notification_manager.py
+++ Day 39/notification_manager.py
@@ -2,6 +2,7 @@ ACCOUNT_SID = 'AC90ea8a134dc896d037599c43b331c745'
 AUTH_TOKEN = 'e80296aaad30a952d38f4ec0be676952'
 PHONE_NUMBER  = '+13092206108'
 from twilio.rest import Client
+import smtplib
 
 class NotificationManager:
     
@@ -16,3 +17,14 @@ class NotificationManager:
         )
         # Prints if successfully sent.
         print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP('EMAIL_PROVIDER_SMTP_ADDRESS') as connection:
+            connection.starttls()
+            connection.login('MY_EMAIL', 'MY_PASSWORD')
+            for email in emails:
+                connection.sendmail(
+                    from_addr='MY_EMAIL',
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
